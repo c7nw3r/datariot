66477659a6e0527fad3c5e8f4ceb1a745ce3f91f
c7nw3r <christian.weber@leftshift.one>
2024-01-31 01:24:47 +0100

    landscape support added to bounding box filter

--- datariot/parser/pdf/bbox/bbox_filter.py
+++ datariot/parser/pdf/bbox/bbox_filter.py
@@ -11,7 +11,12 @@ class CoordinatesBoundingBoxFilter:
         self.max_y = max_y
 
     def __call__(self, page: Page, bboxes: List[PdfTextBox]) -> List[PdfTextBox]:
+        is_landscape = page.layout.width > page.layout.height
+
         def _filter(bbox):
+            if is_landscape:
+                return True
+
             expr1 = bbox.y1 <= self.max_y if self.max_y is not None else True
             expr2 = bbox.y1 >= self.min_y if self.min_y is not None else True
 
