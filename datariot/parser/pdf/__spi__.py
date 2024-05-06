f672d232d9dae513d60498b3a3a4686ff76d7bf2
Thomas Kloiber <thomas.kloiber@leftshift.one>
2024-05-06 08:08:07 +0200

    Add docstrings

--- datariot/parser/pdf/__spi__.py
+++ datariot/parser/pdf/__spi__.py
@@ -12,26 +12,38 @@ class BBoxConfig:
     filter_min_y: Optional[int] = 50
     filter_max_y: Optional[int] = 710
     filter_must_regexes: List[RegexPattern] = field(default_factory=list)
-    filter_must_not_regexes: List[RegexPattern] = field(default_factory=lambda: [RegexPattern(r"^\s*$")])
-    extract_words_extra_attrs: List[str] = field(default_factory=lambda: ["fontname", "size"])
+    filter_must_not_regexes: List[RegexPattern] = field(
+        default_factory=lambda: [RegexPattern(r"^\s*$")]
+    )
+    extract_words_extra_attrs: List[str] = field(
+        default_factory=lambda: ["fontname", "size"]
+    )
     extract_words_keep_blank_chars: bool = True
     merge_same_font_name: bool = True
     merge_same_font_size: bool = True
     merge_same_font_weight: bool = True
-    # always merge boxes with spaces only irrespective of font
+
     merge_blank_always: bool = True
-    # always merge boxes with single non-space character irrespective of font
+    """always merge boxes which contain only whitespaces, irrespective of font"""
+
     merge_single_char_always: bool = True
+    """always merge boxes with a single non-space character irrespective of font"""
+
     merge_blank_consecutive_lines: bool = True
-    # always merge next word to current bounding box if current bounding box has most common font
+
     merge_same_line_always_after_most_common: bool = True
+    """always merge next word to current bounding box if current bounding box has
+    most common font"""
+
     merge_same_line_tolerance: int = 3
     merge_x_tolerance: int = 10
     merge_y_tolerance: int = 10
     columns_split: bool = True
     columns_gap: int = 2
     columns_min_lines: int = 2
-    columns_split_fonts: List[FontSpecification] = field(default_factory=lambda: ["most_common"])
+    columns_split_fonts: List[FontSpecification] = field(
+        default_factory=lambda: ["most_common"]
+    )
     sorter_fuzzy: bool = False
     sorter_y_tolerance: int = 5
     parser_x_tolerance: int = 3
@@ -50,10 +62,6 @@ class PDFParserConfig:
 
 
 class ParsedPDF(Parsed):
-
     def to_json(self):
         formatter = JSONPDFFormatter()
-        return {
-            "path": self.path,
-            "bboxes": [b.render(formatter) for b in self.bboxes]
-        }
+        return {"path": self.path, "bboxes": [b.render(formatter) for b in self.bboxes]}
