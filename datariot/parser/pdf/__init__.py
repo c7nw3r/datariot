2e97ba46e633500df5f09f63d4c2e137042d2f0b
Thomas Kloiber <thomas.kloiber@leftshift.one>
2024-06-13 20:43:53 +0200

    Add parsed pdf to init

--- datariot/parser/pdf/__init__.py
+++ datariot/parser/pdf/__init__.py
@@ -1,3 +1,4 @@
+from .__spi__ import ParsedPDF
 from .pdf_formatter import HeuristicPDFFormatter
 from .pdf_model import PDFColumnTextBox, PDFImageBox, PDFOcrBox, PDFTableBox, PDFTextBox
 from .pdf_parser import PDFParser, PDFParserConfig
