402b22e0d5aaceff0ad4affed9d85765719d5394
Thomas Kloiber <thomas.kloiber@leftshift.one>
2024-06-13 20:24:29 +0200

    Add heuristic formatter to init

--- datariot/parser/pdf/__init__.py
+++ datariot/parser/pdf/__init__.py
@@ -1,7 +1,3 @@
-from .pdf_model import PDFImageBox
-from .pdf_model import PDFOcrBox
-from .pdf_model import PDFTableBox
-from .pdf_model import PDFTextBox
-from .pdf_model import PDFColumnTextBox
-from .pdf_parser import PDFParserConfig
-from .pdf_parser import PDFParser
+from .pdf_formatter import HeuristicPDFFormatter
+from .pdf_model import PDFColumnTextBox, PDFImageBox, PDFOcrBox, PDFTableBox, PDFTextBox
+from .pdf_parser import PDFParser, PDFParserConfig
