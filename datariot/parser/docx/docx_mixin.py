fdd0cf9a61eb12c173d8375cc216b40cd126d580
Thomas Kloiber <thomas.kloiber@leftshift.one>
2024-06-15 15:55:25 +0200

    Add core properties extraction for docx

--- datariot/parser/docx/docx_mixin.py
+++ datariot/parser/docx/docx_mixin.py
@@ -9,18 +9,22 @@ from docx.document import Document as DocxDocument
 from docx.oxml.table import CT_Tbl
 from docx.oxml.text.paragraph import CT_P
 from docx.section import _Header
-from docx.table import _Cell, Table
+from docx.table import Table, _Cell
 from docx.text.paragraph import Paragraph, Run
 
 from datariot.__spi__.error import DataRiotException
 from datariot.__util__.array_util import flatten
 from datariot.__util__.image_util import from_base64
-from datariot.parser.docx.docx_model import DocxTextBox, DocxTableBox, DocxImageBox, DocxListBox
+from datariot.parser.docx.docx_model import (
+    DocxImageBox,
+    DocxListBox,
+    DocxTableBox,
+    DocxTextBox,
+)
 
 
 # noinspection PyMethodMayBeStatic
 class DocumentMixin:
-
     def has_numbering(self, paragraph: Paragraph):
         return len(paragraph._element.xpath("w:pPr/w:numPr/w:ilvl/@w:val")) > 0
 
@@ -68,21 +72,30 @@ class DocumentMixin:
                     writer.writerow(cell.text for cell in row.cells)
                 vf.seek(0)
 
-                rows = list(csv.reader(vf, delimiter=','))
+                rows = list(csv.reader(vf, delimiter=","))
                 elements.append(DocxTableBox(root_name, rows, paragraphs))
 
         return elements
 
     def parse_images(self, root_name: str, document, run: Run):
         xmlstr = str(run.element.xml)
-        my_namespaces = dict([node for _, node in ElementTree.iterparse(io.StringIO(xmlstr), events=['start-ns'])])
+        my_namespaces = dict(
+            [
+                node
+                for _, node in ElementTree.iterparse(
+                    io.StringIO(xmlstr), events=["start-ns"]
+                )
+            ]
+        )
         root = ET.fromstring(xmlstr)
-        if 'pic:pic' in xmlstr:
-            for pic in root.findall('.//pic:pic', my_namespaces):
+        if "pic:pic" in xmlstr:
+            for pic in root.findall(".//pic:pic", my_namespaces):
                 cNvPr_elem = pic.find("pic:nvPicPr/pic:cNvPr", my_namespaces)
                 name_attr = cNvPr_elem.get("name")
                 blip_elem = pic.find("pic:blipFill/a:blip", my_namespaces)
-                embed_attr = blip_elem.get("{http://schemas.openxmlformats.org/officeDocument/2006/relationships}embed")
+                embed_attr = blip_elem.get(
+                    "{http://schemas.openxmlformats.org/officeDocument/2006/relationships}embed"
+                )
                 document_part = document.part
 
                 if embed_attr not in document_part.related_parts:
@@ -104,7 +117,9 @@ class DocumentMixin:
 
     def parse_custom_properties(self, path: str):
         import zipfile
+
         import lxml.etree
+
         zipped_file = zipfile.ZipFile(path)
         opened_file = zipped_file.open("docProps/custom.xml")
         xml = lxml.etree.parse(opened_file)
@@ -117,3 +132,34 @@ class DocumentMixin:
             name_to_value[element.attrib["name"]] = element[0].text
 
         return name_to_value
+
+    def parse_core_properties(self, document: DocxDocument) -> dict:
+        core_property_names = [
+            "title",
+            "subject",
+            "author",
+            "keywords",
+            "last_modified_by",
+            "last_printed",
+            "created",
+            "modified",
+            "category",
+            "comments",
+            "content_status",
+            "identifier",
+            "language",
+            "revision",
+            "version",
+        ]
+
+        return {
+            prop: str(getattr(document.core_properties, prop))
+            for prop in core_property_names
+            if prop is not None and prop != ""
+        }
+
+    def parse_properties(self, path: str, document: DocxDocument) -> dict:
+        custom_properties = self.parse_custom_properties(path)
+        core_properties = self.parse_core_properties(document)
+
+        return core_properties | custom_properties
--- datariot/parser/docx/docx_parser.py
+++ datariot/parser/docx/docx_parser.py
@@ -1,14 +1,13 @@
 import logging
 from typing import Iterator
 
-from datariot.__spi__.error import DataRiotImportException, DataRiotException
-from datariot.__spi__.type import Parsed, Parser, FileFilter
+from datariot.__spi__.error import DataRiotException, DataRiotImportException
+from datariot.__spi__.type import FileFilter, Parsed, Parser
 from datariot.__util__.io_util import get_files
 from datariot.parser.docx.docx_mixin import DocumentMixin
 
 
 class DocxParser(Parser, DocumentMixin):
-
     def __init__(self):
         try:
             from docx import Document
@@ -25,14 +24,17 @@ class DocxParser(Parser, DocumentMixin):
 
         elements = [
             *self.parse_elements(document, document.sections[0].header),
-            *self.parse_elements(document, document)
+            *self.parse_elements(document, document),
         ]
 
-        properties = self.parse_custom_properties(path)
+        properties = self.parse_properties(path, document)
+
         return Parsed(path, elements, properties)
 
     @staticmethod
-    def parse_folder(path: str, file_filter: FileFilter = lambda _: True) -> Iterator[Parsed]:
+    def parse_folder(
+        path: str, file_filter: FileFilter = lambda _: True
+    ) -> Iterator[Parsed]:
         for file in get_files(path, ".docx"):
             try:
                 if file_filter(file):
