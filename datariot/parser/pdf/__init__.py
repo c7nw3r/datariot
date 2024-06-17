2917a5163c5968708e5afa5a1a83d4c466c2c01d
Thomas Kloiber <thomas.kloiber@leftshift.one>
2024-06-17 09:13:19 +0200

    Add config classes to init

--- datariot/parser/pdf/__init__.py
+++ datariot/parser/pdf/__init__.py
@@ -1,4 +1,12 @@
-from .__spi__ import BBoxConfig, ParsedPDF, PDFParserConfig
+from .__spi__ import (
+    BBoxConfig,
+    BoxFilterSizeConfig,
+    OcrConfig,
+    ParsedPDF,
+    PDFParserConfig,
+    TableBoxConfig,
+    TextBoxConfig,
+)
 from .pdf_formatter import HeuristicPDFFormatter
 from .pdf_model import PDFColumnTextBox, PDFImageBox, PDFOcrBox, PDFTableBox, PDFTextBox
 from .pdf_parser import PDFParser
