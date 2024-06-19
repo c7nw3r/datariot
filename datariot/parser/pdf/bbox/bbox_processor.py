eea1b52c4a1c805e53a39f1e66acdce8bece8951
Thomas Kloiber <thomas.kloiber@leftshift.one>
2024-06-19 16:20:19 +0200

    Relax strictness for pdf text box re-cropping

--- datariot/parser/pdf/bbox/bbox_processor.py
+++ datariot/parser/pdf/bbox/bbox_processor.py
@@ -10,7 +10,7 @@ class ReCropTextExtractionBBoxProcessor(BoundingBoxProcessor):
     def __call__(self, page: Page, bboxes: List[PDFTextBox]) -> List[PDFTextBox]:
         processed = []
         for box in bboxes:
-            crop = page.crop(tuple(box))
+            crop = page.crop(tuple(box), strict=False)
             new_box = box.with_text(crop.extract_text())
             processed.append(new_box)
 
