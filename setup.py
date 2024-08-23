b09e3f34c1576b2adc917cb200eb23b1b81d4d0f
c7nw3r <christian.weber@leftshift.one>
2024-08-23 15:38:51 +0200

    project version set to 0.6.4

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages, setup
 
 long_description = Path(__file__).with_name("README.md").read_text()
 
-version = "0.6.3"
+version = "0.6.4"
 setup(
     name="datariot",
     packages=find_packages(exclude=("test")),
