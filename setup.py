a3407b11e6ae9f262201313351fa044431c4e05a
c7nw3r <christian.weber@leftshift.one>
2024-08-23 15:34:06 +0200

    project version set to 0.6.3

--- setup.py
+++ setup.py
@@ -1,15 +1,13 @@
 import os
-
 # read the contents of your README file
 from pathlib import Path
 
 import pkg_resources
 from setuptools import find_packages, setup
 
-
 long_description = Path(__file__).with_name("README.md").read_text()
 
-version = "0.6.2"
+version = "0.6.3"
 setup(
     name="datariot",
     packages=find_packages(exclude=("test")),
