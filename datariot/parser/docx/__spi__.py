00a8e5bba47d61edeb1923282ed34cbdeb9c205c
Thomas Kloiber <thomas.kloiber@leftshift.one>
2024-08-28 12:27:47 +0200

    Fix mutable default

--- datariot/parser/docx/__spi__.py
+++ datariot/parser/docx/__spi__.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 from datariot.__spi__.type import Parsed
 from datariot.parser.__spi__ import BoxFilterSizeConfig
@@ -8,8 +8,8 @@ from datariot.parser.docx.docx_model import DocxTextBox
 @dataclass
 class DocxParserConfig:
     include_images: bool = True
-    image_filter_box_size: BoxFilterSizeConfig = BoxFilterSizeConfig(
-        min_width=30, min_height=30
+    image_filter_box_size: BoxFilterSizeConfig = field(
+        default_factory=lambda: BoxFilterSizeConfig(min_width=30, min_height=30)
     )
     media_use_uuid: bool = True
 
