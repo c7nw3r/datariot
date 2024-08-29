118d3d009ccdf95557998887433763333cc7ee8b
c7nw3r <christian.weber@leftshift.one>
2024-08-29 12:33:23 +0200

    keyword extractor modified, topic extractor renamed to title extractor

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages, setup
 
 long_description = Path(__file__).with_name("README.md").read_text()
 
-version = "0.6.7"
+version = "0.6.8"
 setup(
     name="datariot",
     packages=find_packages(exclude=("test")),
