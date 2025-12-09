01ddef3fbc7fbaba510a5e507a0a4f426b807f1b
Thomas Kloiber <thomas.kloiber@leftshift.one>
2025-12-09 16:32:55 +0100

    chore(release): Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import find_packages, setup
 
 long_description = Path(__file__).with_name("README.md").read_text()
 
-version = "0.6.32"
+version = "0.6.33"
 setup(
     name="datariot",
     packages=find_packages(exclude=("test")),
